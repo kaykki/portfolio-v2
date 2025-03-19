956200aa8ecaf083d27c95ecf24c93be8806b4cd
Kaki Kagatan <kakikagatan@outlook.com>
2025-03-18 23:52:36 -0700

    added logo and improved layout of header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import ProjectsList from '@/components/ProjectsList';
 
 export default function Home() {
 	return (
-		<div>
-			<h1>{Header("home")}</h1>
-			<h1 className="text-2xl">Home</h1>
+		<main>
+			<h1>{Header("Kaki Kagatan")}</h1>
 			<ProjectsList />
-			<Footer />
-		</div>
+		</main>
 	);
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,13 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
 export default function Header(title: string) {
-    return <header><h1>{title}</h1></header>
+    return (
+        <header className='flex flex-row items-center gap-2'>
+            <Link href={"/"}>
+                <Image src='/logo.svg' alt="logo" width={36} height={43} />
+            </Link>
+            <h1>{title}</h1>
+        </header>
+    );
 }
