c3ac528c804697e82bfec027470aa72905f25b77
Kaki Kagatan <kakikagatan@outlook.com>
2025-03-23 00:55:05 -0700

    added tech stack list for each project in index page

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -10,16 +10,21 @@ const gabrito = Gabarito({
 export default async function ProjectsList() {
 
     const projects = await fetchProjects();
-
+    
     return (
         <section className="flex gap-4 flex-col card-coloured order-5 col-span-2">
             <h1 className={`${gabrito.className} text-2xl`}>Projects</h1>
             <ul className="flex flex-col gap-4 items-center justify-center">
                 {projects.map((project: any) => (
-                    <li key={project.id} className="w-[326px] h-[79px] relative">
+                    <li key={project.id} className="w-[326px] h-[79px] relative p-4">
                         <Link href={`/projects/${project.id}`}>
                             <h2 className={`${gabrito.className} absolute text-primary z-10`}>{project.title.rendered}</h2>
                             <Image src={project._embedded['wp:featuredmedia'][0].source_url} alt={project.title.rendered} fill loading="lazy" className="object-cover rounded-md"/>
+                            <ul className="absolute bottom-0 flex gap-4">
+                                {project.acf.development.tools.map((tools: any) => (
+                                    <li key={tools.term_id} className="text-[10px]">{tools.name}</li>
+                                ))}
+                            </ul>
                         </Link>
                     </li>
                 ))}
