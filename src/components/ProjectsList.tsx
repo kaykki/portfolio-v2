4418fa6b9c47641d760ff8d3a0099abbf42e24d3
Kaki Kagatan <kakikagatan@outlook.com>
2025-03-23 19:30:43 -0700

    added a tailwind component for tech stacks called stack and improved project list layout

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -2,15 +2,20 @@ import { fetchProjects } from "@/utils/api";
 import Link from "next/link";
 import Image from "next/image";
 import { Gabarito } from 'next/font/google';
+import { Fredoka } from 'next/font/google';
 
 const gabrito = Gabarito({
     weight: '600'
 })
 
+const fredoka = Fredoka({
+    weight: '300'
+})
+
 export default async function ProjectsList() {
 
     const projects = await fetchProjects();
-    
+
     return (
         <section className="flex gap-4 flex-col card-coloured order-5 col-span-2">
             <h1 className={`${gabrito.className} text-2xl`}>Projects</h1>
@@ -18,13 +23,16 @@ export default async function ProjectsList() {
                 {projects.map((project: any) => (
                     <li key={project.id} className="w-[326px] h-[79px] relative p-4">
                         <Link href={`/projects/${project.id}`}>
-                            <h2 className={`${gabrito.className} absolute text-primary z-10`}>{project.title.rendered}</h2>
-                            <Image src={project._embedded['wp:featuredmedia'][0].source_url} alt={project.title.rendered} fill loading="lazy" className="object-cover rounded-md"/>
-                            <ul className="absolute bottom-0 flex gap-4">
-                                {project.acf.development.tools.map((tools: any) => (
-                                    <li key={tools.term_id} className="text-[10px]">{tools.name}</li>
-                                ))}
-                            </ul>
+                            <Image src={project._embedded['wp:featuredmedia'][0].source_url} alt={project.title.rendered} fill loading="lazy" className="object-cover rounded-md" />
+                            <div className="absolute">
+                                <h2 className={`${gabrito.className} text-primary z-10`}>{project.title.rendered}</h2>
+                                <ul className="flex gap-2 items-center justify-center z-10">
+                                    {project.acf.development.tools.map((tools: any) => (
+                                        <li key={tools.term_id} className={`${fredoka.className} stack`}>{tools.name}</li>
+                                    ))}
+                                </ul>
+                            </div>
+
                         </Link>
                     </li>
                 ))}
