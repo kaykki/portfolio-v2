c050032b17f04cc15ebd6dedd4ef93cf96bd690d
Kaki Kagatan <kakikagatan@outlook.com>
2025-03-29 21:47:05 -0700

    experimenting on dynamic way to change categories on tech stack

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,19 +1,43 @@
+"use client"
+
+import { useState, useEffect, use } from "react";
 import { fetchTechStack } from "@/utils/api"
 
-export default async function TechStack() {
+export default function TechStack() {
 
-    const about = await fetchTechStack();
+    const [category, setCategory] = useState("");
+    const [stack, setStack] = useState([""]);
 
-    console.log(about);
-    
+    // useEffect(() => {
+
+    //     setStack(["development", "design"])
+
+
+
+    // }, [category]);
+
+    console.log(stack);
+
+
     return (
         <section className='card order-6 col-span-2 relative'>
             <ul className='flex gap-2 absolute top-0 right-4'>
-                <li className='tech-stack-facet'>All</li>
-                <li className='tech-stack-facet'>Development</li>
-                <li className='tech-stack-facet'>Design</li>
+                <li className='tech-stack-facet' onClick={() => setCategory("")}>All</li>
+                <li className='tech-stack-facet' onClick={() => setCategory("development")}>Development</li>
+                <li className='tech-stack-facet' onClick={() => setCategory("design")}>Design</li>
             </ul>
             <h2 className='font-gabarito title mt-12'>Tech Stack</h2>
+
+            {/* {fetchTechStack(category).then((data) => {
+                return (
+                    <ul className='flex gap-2 items-center justify-center'>
+                        {data.map((tools: any) => (
+                            <li key={tools.term_id} className="stack">{tools.name}</li>
+                        ))}
+                    </ul>
+                )
+            }
+            )} */}
         </section>
     )
 }
