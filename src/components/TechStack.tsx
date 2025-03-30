51d7110383bcecb8ac259843cc2dea1ae0d00401
Kaki Kagatan <kakikagatan@outlook.com>
2025-03-29 22:57:07 -0700

    finished implementing tech stack list

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -2,42 +2,32 @@
 
 import { useState, useEffect, use } from "react";
 import { fetchTechStack } from "@/utils/api"
+import { Stack } from "@/utils/types";
 
 export default function TechStack() {
 
     const [category, setCategory] = useState("");
-    const [stack, setStack] = useState([""]);
-
-    // useEffect(() => {
-
-    //     setStack(["development", "design"])
-
-
-
-    // }, [category]);
-
-    console.log(stack);
-
+    const [currentStack, currentSetStack] = useState<Stack[]>([]);
+   
+    useEffect(() => {
+        fetchTechStack(category).then(data => currentSetStack(data));
+    }, [category]);
 
     return (
         <section className='card order-6 col-span-2 relative'>
             <ul className='flex gap-2 absolute top-0 right-4'>
-                <li className='tech-stack-facet' onClick={() => setCategory("")}>All</li>
-                <li className='tech-stack-facet' onClick={() => setCategory("development")}>Development</li>
-                <li className='tech-stack-facet' onClick={() => setCategory("design")}>Design</li>
+                <li className={`${category == "" ? "bg-accent" : ""} tech-stack-facet`} onClick={() => setCategory("")}>All</li>
+                <li className={`${category == "development" ? "bg-accent" : ""} tech-stack-facet`} onClick={() => setCategory("development")}>Development</li>
+                <li className={`${category == "design" ? "bg-accent" : ""} tech-stack-facet`} onClick={() => setCategory("design")}>Design</li>
             </ul>
-            <h2 className='font-gabarito title mt-12'>Tech Stack</h2>
+            <h2 className='font-gabarito title mt-12 mb-4'>Tech Stack</h2>
 
-            {/* {fetchTechStack(category).then((data) => {
-                return (
-                    <ul className='flex gap-2 items-center justify-center'>
-                        {data.map((tools: any) => (
-                            <li key={tools.term_id} className="stack">{tools.name}</li>
-                        ))}
-                    </ul>
-                )
-            }
-            )} */}
+            <ul className="flex flex-wrap gap-2">
+                {currentStack.map((stackItem) =>(
+                    <li key={stackItem.term_id} className="stack">{stackItem.name}</li>
+                ))}
+            </ul>
+            
         </section>
     )
 }
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,3 +2,8 @@ export type Props = {
     projectId: string;
     params: Promise<{projectId: string}>;
 }
+
+export type Stack = {
+    term_id: string;
+    name: string;
+}
