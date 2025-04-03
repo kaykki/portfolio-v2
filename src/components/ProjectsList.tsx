87bf0f46dde10e8fcbe467e46cfd340d49a6ee0f
Kaki Kagatan <kakikagatan@outlook.com>
2025-04-02 23:56:16 -0700

    implemented layout facets for the project list

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -1,35 +1,71 @@
+"use client"
+
+import { useState, useEffect } from "react";
+import { Project } from "@/utils/types";
 import { fetchProjects } from "@/utils/api";
 import Link from "next/link";
 import Image from "next/image";
 
-export default async function ProjectsList() {
+export default function ProjectsList() {
 
-    const projects = await fetchProjects();
+    const [projects, setProjects] = useState<Project[]>([]);
+    const [listLayout, setListLayout] = useState("rows");
+
+    useEffect(() => {
+        fetchProjects().then(data => setProjects(data));
+    }, [])
+
 
     return (
-        <section className="flex gap-4 flex-col card-coloured order-5 col-span-2 relative">
+        <section className="
+            card-coloured
+            order-5
+            flex gap-4 flex-col col-span-2 relative">
 
             <div className="bg-secondary px-4 py-2 flex gap-4 w-fit absolute top-0 right-4 rounded-b-xl">
-                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g>
-                        <path className="facet" d="M7.33333 6.33333H0V-1H7.33333V6.33333ZM16 6.33333H8.66667V-1H16V6.33333ZM7.33333 15H0V7.66667H7.33333V15ZM16 15H8.66667V7.66667H16V15Z" fill="#004D57" />
-                    </g>
-                </svg>
-                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g>
-                        <path d="M16 12V16H0L0 12H16ZM0 6L0 10H16V6L0 6ZM0 -4.76837e-07L0 4L16 4V-4.76837e-07H0Z" fill="#004D57" />
-                    </g>
-                </svg>
+                <div onClick={() => setListLayout("grid")}>
+                    <svg width="16" 
+                         height="16" 
+                         viewBox="0 0 16 16" 
+                         fill="none" 
+                         xmlns="http://www.w3.org/2000/svg"
+                         className={`${listLayout == "grid" ? "fill-accent" : "fill-primary"}`}>
+                        <g>
+                            <path d="M7.33333 6.33333H0V-1H7.33333V6.33333ZM16 6.33333H8.66667V-1H16V6.33333ZM7.33333 15H0V7.66667H7.33333V15ZM16 15H8.66667V7.66667H16V15Z"/>
+                        </g>
+                    </svg>
+                </div>
+                <div onClick={() => setListLayout("rows")}>
+                    <svg width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={`${listLayout == "rows" ? "fill-accent" : "fill-primary"}`}>
+                        <g>
+                            <path d="M16 12V16H0L0 12H16ZM0 6L0 10H16V6L0 6ZM0 -4.76837e-07L0 4L16 4V-4.76837e-07H0Z"/>
+                        </g>
+                    </svg>
+                </div >
+
             </div>
             <h2 className="title mt-8">Projects</h2>
-            <ul className="flex flex-col gap-4 items-center justify-center">
+            <ul className={`grid ${listLayout == "rows" ? "grid-cols-1" : "grid-cols-2"}  gap-4 items-center justify-center`}>
                 {projects.map((project: any) => (
-                    <li key={project.id} className="w-full h-[120px] relative p-4">
+                    <li key={project.id}
+                        className={`relative p-4 w-full ${listLayout == "rows" ? "h-[130px]" : "h-[200px]"}`}
+                    >
                         <Link href={`/projects/${project.id}`}>
                             <div className="absolute inset-0 bg-black rounded-md"></div>
-                            <Image src={project._embedded['wp:featuredmedia'][0].source_url} alt={project.title.rendered} fill loading="lazy" className="object-cover rounded-md opacity-55" />
+                            <Image
+                                src={project._embedded['wp:featuredmedia'][0].source_url}
+                                alt={project.title.rendered}
+                                fill
+                                loading="lazy"
+                                className="object-cover rounded-md opacity-35"
+                            />
                             <svg
-                                className="absolute top-2 right-2"
+                                className="absolute top-4 right-4"
                                 width="16"
                                 viewBox="0 0 24 24"
                                 xmlns="http://www.w3.org/2000/svg">
@@ -41,7 +77,7 @@ export default async function ProjectsList() {
 
                                 {/* Tech Stack */}
                                 <ul className="flex flex-wrap gap-2 items-center justify-center">
-                                    {project.acf.development.tools.slice(0,3).map((tools: any) => (
+                                    {project.acf.development.tools.slice(0, 3).map((tools: any) => (
                                         <li key={tools.term_id} className="stack">{tools.name}</li>
                                     ))}
                                 </ul>
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -10,3 +10,29 @@ export type Stack = {
     };
     name: string;
 }
+
+export type Project = {
+    id: string,
+    link: string,
+    title: {
+        rendered: string
+    };
+    acf: {
+        showcase: {
+
+        };
+        development: {
+
+        };
+        design: {
+
+        };
+        ctas: {
+            links: {
+                title: string;
+                url: string;
+                target: string;
+            }
+        };
+    }
+}
